06738d5627b7d9770edf57ae3c245effbafee2e1
shrjain <shrjain@shrjain-VirtualBox.(none)>
2014-07-25 17:42:44 +0530

    remove .pyc files and add manage.py

--- recruiter/settings.py
+++ recruiter/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Core apps
+    'account',
+    'job_details',
+    'resumes',
 )
 
 MIDDLEWARE_CLASSES = (
