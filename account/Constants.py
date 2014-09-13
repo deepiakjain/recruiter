b38f67dab4791d0c99edd9f61e0cd1a961b29f56
Ansh Jain <jain.scs@gmail.com>
2014-09-13 17:11:57 +0530

    Worked on files and update models

---
+++ account/Constants.py
@@ -0,0 +1,17 @@
+
+EXPERIENCE_CHOICES = ((0, 'Fresher'),
+                      (1, '1 year'),
+                      (2, 'greater then 1, less then 2'),
+                      (3, '2 year'),
+                      (4, 'greater then 2, less then 3'),
+                      (5, '3 year'),
+                      (6, 'greater then 3, less then 4'),
+                      (7, '4 Yrs'),
+                      (8, 'greater then 4, less then 5'),
+                      (9, '5 Yrs'),
+                      (10, 'greater then 5 Yrs'),
+                    )
+
+YES_NO_CHOICES = (('Y', 'Yes'),
+                    ('N', 'No')
+                    )
--- account/models.py
+++ account/models.py
@@ -11,8 +11,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import URLValidator
 
+# project constant
+from account.constants import EXPERIENCE_CHOICES, YES_NO_CHOICES
+
 
 class Qualification(models.Model):
+    """
+    """
+    name = models.CharField(max_length=70, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Technology(models.Model):
     """
     """
     name = models.CharField(max_length=70, unique=True)
@@ -52,20 +64,46 @@ class BaseProfile(models.Model):
         return "User %s from %s" %(self.user.first_name)
 
 
-class JobSeekerProfile(BaseProfile):
+class JobSeeker(BaseProfile):
+    profile_pic = models.ImageField(upload_to='user/profile', null=True)
+    address = models.OneToOneField(Address, null=True)
+
+    def __unicode__(self):
+        return "User %s from %s" %(self.user.first_name)
+
+
+class SeekerCompanyInfo(models.Model):
+    company_name = models.CharField(max_length=70, null=True)
+    website = models.CharField(max_length=90, validators=[URLValidator()], null=True)
+    joining_date = models.DateTimeField('Joining Date')
+    notice_period = models.SmallIntegerField('Notice Period', null=True, help_text='in days only')
+    current_ctc = models.CharField('Current CTC', max_length=30, help_text="Current salary in INR", null=True)
+    job_change = models.CharField(max_length=1, choices=YES_NO_CHOICES, verbose_name="Looking for change")
+
+
+class JobSeekerProfile(models.Model):
+    seeker = models.OneToOneField(JobSeeker, null=True)
+    current_company = models.OneToOneField(SeekerCompanyInfo, null=True)
     resume = models.FileField(upload_to='user/resume', null=True)
     profile_header = models.CharField(max_length=130,
                                       help_text="Shreeyansh Jain Python Developer", null=True)
     qualification = models.ForeignKey(Qualification, null=True)
+    technology = models.ForeignKey(Technology, null=True)
+    experience = models.CharField(max_length=2, choices=EXPERIENCE_CHOICES)
+    expected_ctc = models.CharField(max_length=30, help_text="Expected salary in INR", null=True)
+    current_loc = models.CharField(max_length=30, verbose_name="current location", null=True)
+    relocate = models.CharField(max_length=1, choices=YES_NO_CHOICES, verbose_name="Ready to re-locate in India")
+    free_time = models.CharField(max_length=12, verbose_name="Good time to contact you")
 
     def __unicode__(self):
-        return "User %s from %s" %(self.user.first_name, self.profile_header)
+        return "User %s from %s" %(self.seeker.user.first_name)
+
 
 
 # Recruiter Profile is always created by admin user on the request basis, show that
 # application admin can manage him / her info
 class RecruiterProfile(BaseProfile):
-    company_email = models.EmailField(null=True)
+    company_email = models.EmailField(max_length=90, null=True)
     company = models.ForeignKey(CompanyProfile, null=True)
 
 
