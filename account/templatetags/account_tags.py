581448e1792ab3753151feb3a23ffda0b001bc16
Ansh Jain <jain.scs@gmail.com>
2014-11-16 20:48:33 +0530

    header and footer nav alignment

--- account/templatetags/account_tags.py
+++ account/templatetags/account_tags.py
@@ -3,5 +3,10 @@ register = template.Library()
 
 @register.filter
 def split(str, splitter):
-
     return str.split(splitter)[0]
+
+
+@register.filter
+def request_path(str):
+    data = {'/': 'home'}
+    return 'selected' if str.strip() in data else ''
